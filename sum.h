52524a88769154572723c5ed91cdfae69647d42d
gideon <gndoria@gmail.com>
2017-09-11 11:03:12 +0300

    modified sum.h

--- sum.h
+++ sum.h
@@ -1,14 +1,22 @@
+/**
+@file sum.h
+@author Gideon Ndoria
+*/
 #include <vector>
 
 using namespace std;
 
 namespace Fin{
-double su = 0.0;
-
-double sum(const vector<double> &data){
-	for(auto elem : data){
-		su += elem;
+	double su = 0.0;
+	/**
+	A utility function to sum up the elements of a vector
+	@param The vector to work on
+	@return The sum af the elements in the vector
+	*/
+	double sum(const vector<double> &data){
+		for(auto elem : data){
+			su += elem;
+		}
+		return su;
 	}
-	return su;
-}
 };
